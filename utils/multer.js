44d1b06467ce5400e0f6afb6f8414bf057e7d68c
kunal2807 <jagtiani.kunal2000@gmail.com>
2021-06-22 17:42:06 +0530

    fix image upload

--- utils/multer.js
+++ utils/multer.js
@@ -2,15 +2,14 @@ import fs from 'fs'
 import multer from 'multer'
 const storageImg = multer.diskStorage({
   destination: function (req, res, cb) {
-    console.log('===>lalala', req.body)
-    let dir = `public`
+    let dir = `public/${req.user._id}`
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir)
     }
     cb(null, dir)
   },
   filename: function (req, file, cb) {
-    console.log('file: ', file)
+    console.log('body: ', req.body)
     cb(null, Date.now() + file.originalname)
   },
 })
